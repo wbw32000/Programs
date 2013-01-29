b6ac04dc41a76d6e2c81bb4cb2a38f77a51dbaff
wbw32000 <wbw@programmer.net>
2013-01-29 14:05:20 -0800

    Create predefined.php

---
+++ predefined.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Predefined Variables</title>
+</head>
+<body>
+<pre>
+<?php // Script 2.1 - predefined.php
+print "William BW (wbw(at)programmer.net)<br />";
+print_r($_SERVER);
+?>
+</pre>
+</body>
+</html>
