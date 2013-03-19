0f4a729564b8f33fdf7a4e1f7e188763d6ed4766
wbw32000 <wbw@programmer.net>
2013-03-19 19:55:30 -0300

    Create ch8Pcakes.php

---
+++ ch8Pcakes.php
@@ -0,0 +1,49 @@
+ <?php // ch8Pcakes.php
+ /* This page lists J.D. Salinger's bibliography. */
+
+   print "<p>Ch8/ch8Pcakes.php, William BW (wbw(at)programmer.net)</p>";
+
+   // Set the page title and include the header file:
+    define ('TITLE', 'Cakes');
+    include('templates/ch8Pheader.html');
+
+   // Leave the PHP section to display lots of HTML:
+  ?>
+
+  <h2>Cakes</h2>
+  <ul>
+    <li>Carrot cake</li>
+    <li>chocolate brownies</li>
+    <li>Chocolate brownies (gluten-free and wheat)</li>
+    <li>chocolate cake</li>
+    <li>Chocolate cake</li>
+    <li>creme de menthe brownies</li>
+    <li>Crème de menthe brownies</li>
+    <li>german chocolate cake</li>
+    <li>German chocolate cake</li>
+    <li>Lemon cake</li>
+    <li>Lemony vanilla cupcakes (gluten-free and wheat)</li>
+    <li>Naniamo bars</li>
+    <li>Pineapple upside down cake</li>
+    <li>Mocha Chocolate Cake</li>
+    <li>Brazil Nut and Banana Brownie</li>
+    <li>Chocolate Banana Custard Cake</li>
+    <li>Chocolate Cake</li>
+    <li>Fruity Marmalade Cake</li>
+    <li>Oaty Banana Cake</li>
+    <li>Banana Chocolate Cake</li>
+    <li>Mango Cake</li>
+    <li>Spiced Apple Cake</li>
+    <li>Ann's Blooming Lovely Chocolate Orange Cheesecake</li>
+    <li>Cherry and Almond Cake</li>
+    <li>Carrot Cake</li>
+    <li>Rich Plain Sponge Cake</li>
+    <li>Apple Rolypoly</li>
+    <li>Chocolate Celebration Cake</li>
+    <li>Chocolate Sponge Cake</li>
+    <li>Fruit Cake</li>
+    <li>Vanilla Sponge Cake</li>
+    <li>Black Forest Cherry Cake</li>
+  </ul>
+
+ <?php include('templates/ch8Pfooter.html'); ?>
