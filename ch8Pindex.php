5bb7fbe5fa10d0ff5687c50890347127371f57ac
wbw32000 <wbw@programmer.net>
2013-03-19 19:56:06 -0300

    Create ch8Pindex.php

---
+++ ch8Pindex.php
@@ -0,0 +1,19 @@
+<?php // ch8Pindex.php
+
+  print "<p>Ch8/ch8Pindex.php, William BW (wbw(at)programmer.net)</p>";
+
+ /* This is the home page for this site. It uses templates to create the layout. */
+
+ // Include the header:
+  include('templates/ch8Pheader.html');
+ // Leave the PHP section to display lots of HTML:
+ ?>
+
+ <h2>Welcome to a Cakes Fan Club</h2>
+  <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay.</p>
+ <h2>Another Header</h2>
+  <p>Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
+
+ <?php // Return to PHP.
+  include('templates/ch8Pfooter.html');  // Include the footer.
+ ?>
